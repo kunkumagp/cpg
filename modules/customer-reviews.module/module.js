9f652e15f21a3ff88349766fd935543e80712da1
Kunkuma Geeth <kunkumagp@gmail.com>
2024-02-01 14:14:01 +0530

    .

--- modules/customer-reviews.module/module.js
+++
@@ -1,7 +0,0 @@
-var maxHeight = 0;
-
-jQuery(".review-box").each(function(){
-   if (jQuery(this).height() > maxHeight) { maxHeight = jQuery(this).height(); }
-});
-
-jQuery(".review-box").height(maxHeight);
